2f1ab70a1485eb21754b0ed10d83876e6e1034ef
Cathrine Vaage <cathrine.vaage@gmail.com>
2020-09-03 14:55:23 +0200

    Adds matrix mapping in fields

--- src/Field.php
+++ src/Field.php
@@ -3,8 +3,9 @@
 namespace Netflex\Structure;
 
 use Carbon\Carbon;
-use Netflex\Support\Accessors;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Support\Collection;
+use Netflex\Support\Accessors;
 
 /**
  * @property-read int $id
@@ -21,39 +22,96 @@ class Field implements CastsAttributes
 {
   use Accessors;
 
-  protected $attributes = [];
-  protected $type = 'string';
+  protected $type;
 
-  public function __construct($attributes = [])
+  public function __construct ($typeOrAttributes = 'string')
   {
-    if (is_string($attributes)) {
-      $this->type = $attributes;
+    if (is_string($typeOrAttributes)) {
+      $this->type = $typeOrAttributes;
+      $this->attributes = [];
+    } else if (is_array($typeOrAttributes)) {
+      $this->type = $typeOrAttributes['type'];
+      $this->attributes = $typeOrAttributes;
     }
-
-    $this->attributes = is_array($attributes) ? $attributes : [];
   }
 
-  public function getIdAttribute($id)
+  public function getIdAttribute ($id)
   {
     return (int) $id;
   }
 
-  public function getCollectionIdAttribute($collectionId)
+  public function getCollectionIdAttribute ($collectionId)
   {
     return (int) $collectionId;
   }
 
-  public function getSortingAttribute($sorting)
+  public function getSortingAttribute ($sorting)
   {
     return (int) $sorting;
   }
 
-  public function get($model, $key, $value, $attributes)
+  /**
+   * @param Collection $fields
+   * @param Collection $keys
+   * @return static
+   */
+  protected function findField (Collection $fields, Collection $keys)
+  {
+    $key = $keys->shift();
+
+    $fieldData = $fields->first(function ($field) use ($key) {
+      return is_object($field)
+        ? $field->alias === $key
+        : $field['alias'] == $key;
+    });
+
+    $field = new static($fieldData->attributes ?? $fieldData);
+
+    if ($keys->count() === 0) {
+      return $field;
+    }
+
+    $subFields = $field->type === 'matrix'
+      ? $field->blocks
+      : $field->fields;
+
+    return $this->findField(Collection::make($subFields), $keys);
+  }
+
+  /**
+   * @param Model $model
+   * @param string|array $key
+   * @return static
+   */
+  protected function getField (Model $model, $key)
+  {
+    return $this->findField($model->structure->fields, Collection::make($key));
+  }
+
+  protected function getBlockField (array $block, Field $field) {
+    $blockFieldAttributes = Collection::make($field->blocks)
+      ->first(function ($blockFieldAttributes) use ($block) {
+        return $blockFieldAttributes['alias'] === $block['type'];
+      });
+
+    return new static($blockFieldAttributes);
+  }
+
+  /**
+   * @param Model $model
+   * @param string|array $keys
+   * @param mixed $value
+   * @param array $attributes
+   * @return array|bool|Carbon|float|int|mixed|EditorBlocks|JSON|null
+   */
+  public function get ($model, $keys, $value, $attributes)
   {
     switch ($this->type) {
       case 'checkbox':
         return boolval(intval($value));
       case 'integer':
+      case 'entry':
+      case 'customer':
         return intval($value);
       case 'float':
         return floatval($value);
@@ -70,14 +128,54 @@ class Field implements CastsAttributes
         return $value ? Carbon::parse($value)->startOfDay() : null;
       case 'datetime':
         return $value ? Carbon::parse($value) : null;
+      case 'matrix':
+        $field = $this->getField($model, $keys);
+
+        return Collection::make($value)
+          ->map(function ($block) use ($model, $keys, $value, $field) {
+            $blockField = $this->getBlockField($block, $field);
+
+            return $blockField->get($model, [$keys, $block['type']], $block, $blockField->attributes);
+          })
+          ->toArray();
+      case 'matrix_block':
+        $field = $this->getField($model, $keys);
+
+        return Collection::make($field->fields)
+          ->mapWithKeys(function ($fieldAttributes) use ($model, $value, $keys) {
+            $field = new static($fieldAttributes);
+
+            $fieldName = $fieldAttributes['alias'];
+
+            $fieldValue = $value[$fieldName] ?? null;
+
+            return [
+              $fieldName => $field->get($model, array_merge($keys, [$fieldName]), $fieldValue, $fieldAttributes),
+            ];
+          })
+          ->merge([
+            'type' => $value['type'],
+          ])
+          ->toArray();
       default:
         return $value;
     }
   }
 
-  public function set($model, $key, $value, $attributes)
+  public function set ($model, $keys, $value, $attributes)
   {
+    $key = Collection::make($keys)->pop();
+
     switch ($this->type) {
+      case 'checkbox':
+        $value = $value ? '1' : '0';
+        break;
+      case 'integer':
+      case 'entry':
+      case 'customer':
+      case 'float':
+        $value = (string) $value;
+        break;
       case 'tags':
       case 'entries':
       case 'customers':
@@ -95,9 +193,51 @@ class Field implements CastsAttributes
       case 'datetime':
         $value = $value instanceof Carbon ? $value->toDateTimeString() : $value;
         break;
+      case 'matrix':
+        $field = $this->getField($model, $keys);
+
+        $value = Collection::make($value)
+          ->map(function ($block) use ($model, $keys, $value, $field) {
+            $blockField = $this->getBlockField($block, $field);
+
+            $blockKey = $block['type'];
+
+            return $blockField->set($model, [$keys, $blockKey], $block, $blockField->attributes)[$blockKey];
+          })
+          ->toArray();
+
+        break;
+      case 'matrix_block':
+        $field = $this->getField($model, $keys);
+
+        $value = Collection::make($field->fields)
+          ->mapWithKeys(function ($fieldAttributes) use ($model, $value, $keys) {
+            $field = new static($fieldAttributes);
+
+            $fieldName = $fieldAttributes['alias'];
+
+            $fieldValue = $value[$fieldName] ?? null;
+
+            $foo = [
+              $fieldName => $field->set($model, array_merge($keys, [$fieldName]), $fieldValue, $fieldAttributes)[$fieldName],
+            ];
+
+            return $foo;
+          })
+          ->merge([
+            'type' => $value['type'],
+          ])
+          ->toArray();
+        break;
       default:
         break;
     }
+
     return [$key => $value];
+  }
+
+  public function __debugInfo ()
+  {
+    return $this->attributes;
   }
 }
