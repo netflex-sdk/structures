5961b8552e3af03de8665b431e7edf0fd6ceca1b
Thomas Alrek <thomas@alrek.no>
2021-02-12 14:31:26 +0100

    Fix JSON serialization of File

--- src/File.php
+++ src/File.php
@@ -7,7 +7,10 @@ use ArrayAccess;
 use Netflex\Support\Accessors;
 use Netflex\Pages\Contracts\MediaUrlResolvable;
 
-class File implements ArrayAccess, MediaUrlResolvable
+use JsonSerializable;
+use Illuminate\Contracts\Support\Jsonable;
+
+class File implements ArrayAccess, MediaUrlResolvable, JsonSerializable, Jsonable
 {
     use Accessors;
 
@@ -24,11 +27,21 @@ class File implements ArrayAccess, MediaUrlResolvable
     public function __debugInfo()
     {
         $attributes = [];
-        
+
         foreach ($this->attributes as $key => $value) {
             $attributes[$key] = $this->__get($key);
         }
 
         return $attributes;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->attributes;
+    }
+
+    public function toJson($options = 0)
+    {
+        return json_encode($this->jsonSerialize(), $options);
     }
 }
