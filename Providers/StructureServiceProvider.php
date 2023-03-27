509ecd0757c36f8a98f3696eb52cb4b83baa146e
Thomas <thomas@alrek.no>
2023-03-27 18:04:31 +0000

    Improves loading time of StructureServiceProvider

--- Providers/StructureServiceProvider.php
+++ Providers/StructureServiceProvider.php
@@ -6,6 +6,8 @@ namespace Netflex\Structure\Providers;
 use Illuminate\Support\ServiceProvider;
 
 use HaydenPierce\ClassFinder\ClassFinder;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Netflex\Structure\Structure;
 use Throwable;
 
@@ -13,12 +15,25 @@ class StructureServiceProvider extends ServiceProvider
 {
     public function boot ()
     {
-        $models = ClassFinder::getClassesInNamespace('App\Models', ClassFinder::RECURSIVE_MODE);
-        
+        $models = [];
+        $classFinder = fn () => ClassFinder::getClassesInNamespace('App\Models', ClassFinder::RECURSIVE_MODE);
+
+        if(in_array(App::environment(), ['master', 'dev'])) {
+          $models = Cache::rememberForever('', $classFinder);
+        } else {
+          $models = $classFinder();
+        }
+
         foreach ($models as $model) {
             try {
-                if (!Structure::isModelRegistered($model)) {
+              $isModelRegistered = App::measure('isModelRegistered' . $model, function () use ($model) {
+                return !Structure::isModelRegistered($model);
+              });
+
+                if ($isModelRegistered) {
+                  App::measure('registerModel', function () use ($model) {
                     Structure::registerModel($model);
+                  });
                 }
             } catch (Throwable $e) {
                 continue;
