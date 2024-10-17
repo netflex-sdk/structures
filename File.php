a56bf6e043ddd46e3d1b5d56f6a4558366fe2d46
Thomas <thomas@alrek.no>
2024-10-17 08:39:35 +0000

    add getNetflexRoutesPage method to RouteServiceProvider

--- File.php
+++ File.php
@@ -64,7 +64,6 @@ class File implements ArrayAccess, MediaUrlResolvable, JsonSerializable, Arrayab
         }
     }
 
-    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return $this->toArray();
