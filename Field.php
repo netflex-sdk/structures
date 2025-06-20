2877db93b00e3067efa8cc0d4da50f0328c9d9f7
Thomas <thomas@alrek.no>
2025-06-20 13:18:14 +0000

    Merge pull request #64 from netflex-sdk/fix/improve-picture-speed

--- Field.php
+++ Field.php
@@ -122,10 +122,6 @@ class Field implements CastsAttributes
     switch ($this->type) {
       case 'checkbox':
         return boolval(intval($value));
-      case 'checkbox-group':
-        return $value
-          ? array_values(array_filter(explode(',', $value)))
-          : [];
       case 'customer-group':
       case 'entry':
       case 'customer':
@@ -264,7 +260,6 @@ class Field implements CastsAttributes
       case 'entries':
       case 'entriessortable':
       case 'customers':
-      case 'checkbox-group':
         $value = is_array($value) ? implode(',', $value) : $value;
         break;
       case 'editor-small':
